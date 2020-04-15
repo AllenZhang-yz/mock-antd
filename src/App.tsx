faed2b3c9f2ede93b4ccd43e15043501d31aedda
Allen Zhang <allen.zhang018@gmail.com>
2020-04-15 13:43:02 +1000

    finish Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonSize, ButtonType } from './components/Button/Button';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button>Hello</Button>
+        <Button disabled>Disabled Button</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          Primary Large
+        </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+          Small Danger
+        </Button>
+        <Button
+          btnType={ButtonType.Link}
+          href="http://www.baidu.com"
+          target="_blank"
+        >
+          Baidu Link
+        </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>
+          Disabled Link
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -21,6 +37,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;

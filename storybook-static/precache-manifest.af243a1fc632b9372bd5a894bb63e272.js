1f5c4dbf9c775206f2b187e28a688c9f764b8aba
Allen Zhang <allen.zhang018@gmail.com>
2020-04-30 16:42:56 +1000

    upload to github page

---
+++ storybook-static/precache-manifest.af243a1fc632b9372bd5a894bb63e272.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b4380bf655bd268e742359d789b7f1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.94f7a913d7c1f13591c5.bundle.js"
+  },
+  {
+    "url": "runtime~main.94f7a913d7c1f13591c5.bundle.js"
+  },
+  {
+    "revision": "e3a7bc1861f891ff974c",
+    "url": "static/css/main.5979ad69.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.94f7a913d7c1f13591c5.bundle.js"
+  },
+  {
+    "url": "vendors~main.94f7a913d7c1f13591c5.bundle.js.LICENSE.txt"
+  }
+]);

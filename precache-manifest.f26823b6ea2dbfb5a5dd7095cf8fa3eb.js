a1f1d3ce57d6d4c4eeaef6952134a73cb3ef9489
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-30 06:46:12 +0000

    Deploy AllenZhang-yz/mock-antd to github.com/AllenZhang-yz/mock-antd.git:gh-pages

---
+++ precache-manifest.f26823b6ea2dbfb5a5dd7095cf8fa3eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e7944a23c71abf2f175dbedccedcf2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d18093d453e5a3c7804e.bundle.js"
+  },
+  {
+    "url": "runtime~main.d18093d453e5a3c7804e.bundle.js"
+  },
+  {
+    "revision": "02d1c7a7a46e168f9fec",
+    "url": "static/css/main.5979ad69.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.d18093d453e5a3c7804e.bundle.js"
+  },
+  {
+    "url": "vendors~main.d18093d453e5a3c7804e.bundle.js.LICENSE.txt"
+  }
+]);
